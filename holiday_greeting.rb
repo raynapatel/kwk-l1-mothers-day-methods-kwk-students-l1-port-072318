4c27ac77f28ed5835fc09fcc84259400f95abe14
raynapatel <raynapatelpdx@gmail.com>
2018-07-24 18:18:20 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,4 +1,5 @@
-def holiday(recipient = "Mom", sender = "Your Favorite Child", event = "Mother's Day")
+def holiday(recipient = gets.strip, sender = gets.strip, event = gets.strip)
+  puts "What is the event, who is the sender, who is the recipient?"
   puts "Happy #{event}, #{recipient}! - From #{sender}"
 end
 
